5f3e70da16e24d9cb5d38d36e2ade205f4d73978
Aleksandr_HomeVM <shurikkiev90@gmail.com>
2019-01-07 20:17:34 +0200

    1. Добавлена аппликация students_app и инициализирована в setting.py. Страница static/index.html скопированна как students_app/templates/students/students_list.html 2. Созданы вьюшки для перехода на страницу, редактирования, добавления, удаления Студента и Группы 3. Прописаны регулярные выражения в urls.py для переходов(подвязки) вьюшек. 4. В странице students_list.html вместо # вписаны линки для корректных переходов по вьюшке

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -2,9 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'studentsdb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Students urls:
+                       url(r'^$', 'students_app.views.students_list', name='home'),
+                       url(r'^students/add/$', 'students_app.views.students_add', name='students_add'),
+                       url(r'^students/(?P<sid>\d+)/edit/$', 'students_app.views.students_edit', name='students_edit'),
+                       url(r'^students/(?P<sid>\d+)/delete/$', 'students_app.views.students_delete',
+                           name='students_delete'),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+
+                       # Groups urls:
+                       url(r'^groups/$', 'students_app.views.groups_list', name='groups'),
+                       url(r'^groups/add/$', 'students_app.views.groups_add', name='groups_add'),
+                       url(r'^groups/(?P<gid>\d+)/edit/$', 'students_app.views.groups_edit', name='groups_edit'),
+                       url(r'^groups/(?P<gid>\d+)/delete/$', 'students_app.views.groups_delete', name='groups_delete'),
+
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
