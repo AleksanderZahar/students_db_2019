10b53ab0ce879355a7ff0fa07eb0307682bebbc9
Aleksandr_HomeVM <shurikkiev90@gmail.com>
2019-01-20 21:23:49 +0200

    Глава 7-продолжение: 1. регистрация админки 2. пофиксили отображение студентов в админки, добавление класса Мета в модели студента 3. коррекция вьюшки студентов для загрузки их из БД, а не костыли 4. прописали в настройках MEDIA_ROOT, MEDIA_URL для загрузки медиаресурсов из вне нашего приложения, вынесли папку медиа за корень проекта. добавили изображение студента по-умолчанию

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
                        # Students urls:
@@ -13,7 +14,8 @@ urlpatterns = patterns('',
                        # Groups urls:
                        url(r'^groups/$', 'students_app.views.groups.groups_list', name='groups'),
                        url(r'^groups/add/$', 'students_app.views.groups.groups_add', name='groups_add'),
-                       url(r'^groups/(?P<gid>\d+)/edit/$', 'students_app.views.groups.groups_edit', name='groups_edit'),
+                       url(r'^groups/(?P<gid>\d+)/edit/$', 'students_app.views.groups.groups_edit',
+                           name='groups_edit'),
                        url(r'^groups/(?P<gid>\d+)/delete/$', 'students_app.views.groups.groups_delete',
                            name='groups_delete'),
 
@@ -27,3 +29,9 @@ urlpatterns = patterns('',
 
                        url(r'^admin/', include(admin.site.urls)),
                        )
+if DEBUG:
+    # serve files from media folder
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': MEDIA_ROOT}))
