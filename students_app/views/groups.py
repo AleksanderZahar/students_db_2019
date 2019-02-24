04192cf44151f545b661794a47ab901724c9f009
Aleksandr_HomeVM <shurikkiev90@gmail.com>
2019-02-24 20:39:37 +0200

    Глава 7-продолжение: 1. организованы модели в подпакеты, исправленны их импорты во вьюшках и админке

--- students_app/views/groups.py
+++ students_app/views/groups.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from ..models import Group
+from ..models.groups import Group
 
 # Views for Groups:
 
