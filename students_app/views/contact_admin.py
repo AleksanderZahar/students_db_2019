07bedd32e663eb8e0560332e5268f5da9d55d0e3
Aleksandr_HomeVM <shurikkiev90@gmail.com>
2019-03-24 20:08:57 +0200

    Глава 8 - Форма добавления студентов (стр. 329-337): 1. статусные сообщения формы обрабатываются встроенным модулем джанго.сообщения

--- students_app/views/contact_admin.py
+++ students_app/views/contact_admin.py
@@ -5,7 +5,7 @@ from django import forms
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
+from django.contrib import messages
 from studentsdb.settings import ADMIN_EMAIL, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Contact Admin Form
@@ -38,12 +38,16 @@ def contact_admin(request):
             try:
                 send_mail(subject, message, from_email, [ADMIN_EMAIL])
             except Exception:
-                message = u"Під час відправки листа виникла непередбачувана помилка. Спробуйте пізніше"
+                # message = u"Під час відправки листа виникла непередбачувана помилка. Спробуйте пізніше"
+                status_message = messages.error(request, u"Під час відправки листа виникла непередбачувана помилка."
+                                                         u" Спробуйте пізніше")
             else:
-                message = u"Повідомлення успішно відправлено!"
+                # message = u"Повідомлення успішно відправлено!"
+                status_message = messages.success(request, u"Повідомлення успішно відправлено!")
             # redirect to same contact page with success message
-            return HttpResponseRedirect(
-                u'%s?status_message=%s' % (reverse('contact_admin'), message))
+            # return HttpResponseRedirect(
+            #     u'%s?status_message=%s' % (reverse('contact_admin'), message))
+            return HttpResponseRedirect(reverse('contact_admin'), status_message)
     # if there was not POST render blank form
     else:
         form = ContactForm()
